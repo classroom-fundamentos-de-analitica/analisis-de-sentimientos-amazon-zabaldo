c6b92b563908f273f2c355758bc907e6771d2c05
david <david@debian-BULLSEYE-live-builder-AMD64>
2023-06-23 16:42:30 -0500

    r

--- preguntas.py
+++ preguntas.py
@@ -22,7 +22,7 @@ def pregunta_01():
     # Etiquete la primera columna como `msg` y la segunda como `lbl`. Esta función
     # retorna el dataframe con las dos columnas.
     df = pd.read_csv(
-        'amazon_cells_labelled.txt',
+        'amazon_cells_labelled.tsv',
         sep='\t',
         header=None,
         names=['msg', 'lbl'],
