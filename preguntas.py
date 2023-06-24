6e41a0d6248da57a5b0c3947d580eb1bebc8cf53
david <david@debian-BULLSEYE-live-builder-AMD64>
2023-06-23 20:51:12 -0500

    r

--- preguntas.py
+++ preguntas.py
@@ -126,7 +126,7 @@ def pregunta_04():
     # Cree un pipeline que contenga el CountVectorizer y el modelo de BernoulliNB.
     pipeline = Pipeline(
         steps=[
-            ("countVectorizer", countVectorizer),
+            ("CountVectorizer", countVectorizer),
             ("bernoulliNB", BernoulliNB()),
         ],
     )
